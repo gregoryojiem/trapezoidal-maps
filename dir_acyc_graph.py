da55298276922bd1415135b3e91810e4149210d9
HoldenLalumiere <holdenlalumiere@gmail.com>
2024-10-28 15:01:32 -0400

    Added case function headers

--- dir_acyc_graph.py
+++ dir_acyc_graph.py
@@ -2,6 +2,13 @@ class DAG:
     def __init__(self):
         self.head = None
 
+    def resolve_case1(self): return None
+    def resolve_case2(self): return None
+    def resolve_case3(self): return None
+
+    def create_output_matrix(self):
+        return None
+
 
 class Internal:
     def __init__(self, left, right):
