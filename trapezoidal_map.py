2f402ea0c1818a9b8918cf1f842d74642c1fae59
HoldenLalumiere <holdenlalumiere@gmail.com>
2024-10-28 14:02:06 -0400

    Added Doccomment

--- trapezoidal_map.py
+++ trapezoidal_map.py
@@ -16,6 +16,13 @@ class Segment:
 
 class Trapezoid:
     def __init__(self, top_seg, bot_seg, left_vert, right_vert):
+        """
+        Creates a trapezoid
+        :param top_seg: Top line Segment
+        :param bot_seg: Bottom line Segment
+        :param left_vert: Left vertex
+        :param right_vert: Right vertex
+        """
         self.top_seg = top_seg
         self.bot_seg = bot_seg
         self.left_vert = left_vert
