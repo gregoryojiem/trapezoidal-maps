a94bd97bc3bd71c24ab3f456e3bb5e5962bb38ee
HoldenLalumiere <holdenlalumiere@gmail.com>
2024-10-28 14:06:19 -0400

    Added ToStrings

--- trapezoidal_map.py
+++ trapezoidal_map.py
@@ -3,6 +3,9 @@ class Point:
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
 
 class Segment:
     """
@@ -12,6 +15,9 @@ class Segment:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
+
+    def __str__(self):
+        return f"{self.p1}-{self.p2}"
 
 
 class Trapezoid:
