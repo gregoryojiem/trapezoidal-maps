908e75af7122cdf3df043324859d23897af921ff
Gregory Ojiem <gro3228@rit.edu>
2024-10-28 17:25:39 -0400

    Finished point region detection and case handling

--- dir_acyc_graph.py
+++ dir_acyc_graph.py
@@ -1,9 +1,67 @@
-class DAG:
-    def __init__(self):
-        self.head = None
+from dag_case1 import handle_case1
+from dag_case2 import handle_case2
+from dag_case3 import handle_case3
 
-    def add_new_segment(self, segment):
-        pass
+
+class DAG:
+    def __init__(self, bounding_trapezoid):
+        self.head = Node(Leaf(bounding_trapezoid))
+
+    def add_new_segment(self, seg):
+        affected_trapezoids = list(self.find_trapezoids(self.head, seg))
+
+        if len(affected_trapezoids) == 1:
+            handle_case2(seg, affected_trapezoids[0])
+            return
+
+        left_point_region = self.find_point_region(self.head, seg.p1)
+        right_point_region = self.find_point_region(self.head, seg.p2)
+        affected_trapezoids.remove(left_point_region)
+        affected_trapezoids.remove(right_point_region)
+        handle_case1(seg.p1, seg, left_point_region)
+        handle_case1(seg.p2, seg, right_point_region)
+
+        for trapezoid in affected_trapezoids:
+            handle_case3(seg, trapezoid)
+
+    def find_point_region(self, node, point):
+        curr_node = node.data
+
+        if isinstance(curr_node, Leaf):
+            return node
+
+        if isinstance(curr_node, PointNode):
+            if not point.is_right_of(curr_node.point):
+                return self.find_trapezoids(curr_node.left, point)
+            else:
+                return self.find_trapezoids(curr_node.right, point)
+
+        if isinstance(curr_node, SegNode):
+            if point.is_above(curr_node.seg):
+                return self.find_trapezoids(curr_node.left, point)
+            else:
+                return self.find_trapezoids(curr_node.right, point)
+
+    def find_trapezoids(self, node, seg):
+        curr_node = node.data
+
+        if isinstance(curr_node, Leaf):
+            return {node}
+
+        if isinstance(curr_node, PointNode):
+            if not seg.p2.is_right_of(curr_node.point):  # segment is fully left of the point
+                return {self.find_trapezoids(curr_node.left, seg)}
+            elif seg.p1.is_right_of(curr_node.point):  # segment is fully right of the point
+                return {self.find_trapezoids(curr_node.right, seg)}
+            else:  # the point is in between the segment endpoints
+                return {self.find_trapezoids(curr_node.right, seg)} \
+                    .union({self.find_trapezoids(curr_node.left, seg)})
+
+        if isinstance(curr_node, SegNode):
+            if seg.is_above(curr_node.seg):
+                return {self.find_trapezoids(curr_node.left, seg)}
+            else:
+                return {self.find_trapezoids(curr_node.right, seg)}
 
     def create_output_matrix(self):
         return None
@@ -16,7 +74,6 @@ class Node:
 
 class Internal:
     def __init__(self, left, right):
-        super().__init__()
         self.left = left
         self.right = right
 
@@ -27,6 +84,18 @@ class Internal:
             return False
         else:
             raise Exception("Not the parent of the child")
+
+
+class PointNode(Internal):
+    def __init__(self, left, right, point):
+        super().__init__(left, right)
+        self.point = point
+
+
+class SegNode(Internal):
+    def __init__(self, left, right, seg):
+        super().__init__(left, right)
+        self.seg = seg
 
 
 class Leaf:
