2e6ca85df692ff2bed94ab540635fbdbe362c878
HoldenLalumiere <holdenlalumiere@gmail.com>
2024-10-28 16:13:28 -0400

    Updated case2 to handle new dag structure

--- dag_case2.py
+++ dag_case2.py
@@ -9,7 +9,7 @@ def handle_case2(seg, leaf):
     :param seg: Segment that was added to the graph
     :param leaf: Leaf node which contains the trapezoid
     """
-    trap = leaf.trap
+    trap = leaf.data.trap
     u = Leaf(Trapezoid(trap.top_seg, seg, seg.p1, seg.p2))
     d = Leaf(Trapezoid(seg, trap.bot_seg, seg.p1, seg.p2))
     r = Leaf(Trapezoid(trap.top_seg, trap.bot_seg, seg.p2, trap.right_vert))
@@ -19,4 +19,4 @@ def handle_case2(seg, leaf):
     q = Internal(s, r)
     p = Internal(l, q)
     # TODO check that leaf was replaced by the new tree
-    leaf = p
+    leaf.data = p
