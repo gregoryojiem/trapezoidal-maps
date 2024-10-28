8ce693d5047ec802fe3f6610442e26f5acaa3c6d
HoldenLalumiere <holdenlalumiere@gmail.com>
2024-10-28 17:03:07 -0400

    Implemented case1

--- dag_case1.py
+++ dag_case1.py
@@ -3,12 +3,38 @@ from trapezoidal_map import Trapezoid
 
 
 def handle_case1(p, seg, leaf):
-    pass
-    # lr = Node(Leaf(Trapezoid))
-    # if is_left_end_point:
-    #     pl = lr
-    #     pr = s
-    # else:
-    #     pl = s
-    #     pr = lr
-    # p = Internal(pl, pr)
+    trap = leaf.data.trap
+    is_left_end_point = True if seg.p1 == p else False
+
+    if is_left_end_point:
+        up_left = p
+        up_right = seg.p2 if seg.p2.x <= trap.top_seg.x else trap.top_seg.p2
+        down_left = p
+        down_right = seg.p2 if seg.p2.x <= trap.bot_seg.x else trap.top_seg.p2
+    else:
+        up_left = seg.p1 if seg.p1.x >= trap.top_seg.x else trap.top_seg.p1
+        up_right = p
+        down_left = seg.p1 if seg.p1.x >= trap.bot_seg.x else trap.bot_seg.p1
+        down_right = p
+    up = Node(Leaf(Trapezoid(trap.top_seg, seg, up_left, up_right)))
+    down = Node(Leaf(Trapezoid(seg, trap.bot_seg, down_left, down_right)))
+    s = Node(Internal(up, down))
+
+    if is_left_end_point:
+        lr_left = trap.top_seg.p1 if trap.top_seg.p1.x >= trap.bot_seg.p1.x else trap.bot_seg.p1
+        lr_right = p
+    else:
+        lr_left = p
+        lr_right = trap.top_seg.p2 if trap.top_seg.p2.x <= trap.bot_seg.p2.x else trap.bot_seg.p2
+
+    left_or_right = Node(Leaf(Trapezoid(trap.top_seg, trap.bot_seg, lr_left, lr_right)))
+
+    if is_left_end_point:
+        pl = left_or_right
+        pr = s
+    else:
+        pl = s
+        pr = left_or_right
+
+    p = Node(Internal(pl, pr))
+    leaf.data = p
