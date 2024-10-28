ef9c079394ed2118fe3107ccb77432657dac9621
Gregory Ojiem <gro3228@rit.edu>
2024-10-28 14:07:15 -0400

    Fixed bug with bounds

--- visualizations.py
+++ visualizations.py
@@ -13,8 +13,8 @@ def plot_segments(segments, bounds):
         mid_p_y = ((segment.p1.y + segment.p2.y) / 2) + 4
         ax.text(mid_p_x, mid_p_y, f"S{i + 1}", color="dodgerblue")
 
-    ax.set_xlim(bounds.x, bounds.x)
-    ax.set_ylim(bounds.y, bounds.y)
+    ax.set_xlim(bounds[0], bounds[2])
+    ax.set_ylim(bounds[1], bounds[3])
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_title("Line Segments")
