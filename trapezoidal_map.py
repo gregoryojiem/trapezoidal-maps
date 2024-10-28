9c75de4749ef70d275bec374c572c9080f3b73a0
HoldenLalumiere <holdenlalumiere@gmail.com>
2024-10-28 14:00:05 -0400

    Revert "Added deccomment"
    This reverts commit 23e66b56488ec936bed85b3a24f9a0787aa88925.

--- trapezoidal_map.py
+++ trapezoidal_map.py
@@ -4,13 +4,6 @@ class Trapezoid:
     """
 
     def __init__(self, top_seg, bot_seg, left_vert, right_vert):
-        """
-        Creates a trapezoid
-        :param top_seg: Top line Segment
-        :param bot_seg: Bottom line Segment
-        :param left_vert: Left vertex
-        :param right_vert: Right vertex
-        """
         self.top_seg = top_seg
         self.bot_seg = bot_seg
         self.left_vert = left_vert
