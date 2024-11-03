31bd5df42f349c00b4123d28cbb990ee29557664
HoldenLalumiere <holdenlalumiere@gmail.com>
2024-11-03 18:39:22 -0500

    Fixed broken top/bottom traps

--- dag_case1.py
+++ dag_case1.py
@@ -14,14 +14,17 @@ def handle_case1(point, seg, node_leaf):
     is_left_end_point = seg.p1 == point
     if is_left_end_point:
         up_left = point
-        up_right = seg.p2 if not seg.p2.is_right_of(trap.top_seg.p2) else trap.top_seg.p2
+        up_right = seg.p2 if not seg.p2.is_right_of(trap.right_vert) else trap.right_vert
         down_left = point
         down_right = seg.p2 if not seg.p2.is_right_of(trap.bot_seg.p2) else trap.bot_seg.p2
         down = Node(Leaf(Trapezoid(seg, trap.bot_seg, down_left, down_right)))
     else:
-        up_left = seg.p1 if seg.p1.is_right_of(trap.top_seg.p1) else trap.top_seg.p1
+        up_left = seg.p1 if seg.p1.is_right_of(trap.left_vert) else trap.left_vert
         up_right = point
-        down = Node(None) #todo link to the down trap that the left point made
+        # down = Node(None) #todo link to the down trap that the left point made
+        down_left = seg.p1 if seg.p1.is_right_of(trap.bot_seg.p1) else trap.bot_seg.p1
+        down_right = point
+        down = Node(Leaf(Trapezoid(seg, trap.bot_seg, down_left, down_right)))
     up = Node(Leaf(Trapezoid(trap.top_seg, seg, up_left, up_right)))
     s = Node(SegNode(up, down, seg))
 
