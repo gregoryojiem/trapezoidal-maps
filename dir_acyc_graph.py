5b878b254b1cdc65813749409284e609bbc8f260
Gregory Ojiem <gro3228@rit.edu>
2024-11-03 20:29:52 -0500

    Added visualizations for debugging

--- dir_acyc_graph.py
+++ dir_acyc_graph.py
@@ -2,31 +2,51 @@ from dag_structures import Node, PointNode, SegNode, Leaf
 from dag_case1 import handle_case1_left, handle_case1_right
 from dag_case2 import handle_case2
 from dag_case3 import handle_case3
-
+import visualizations
 
 class DAG:
     def __init__(self, bounding_trapezoid):
         self.head = Node(Leaf(bounding_trapezoid))
 
-    def add_new_segment(self, seg):
+    def add_new_segment(self, seg, bbox):
         print("Adding new segment: " + str(seg))
         affected_trapezoids = list(self.find_trapezoids(self.head, seg))
 
         if len(affected_trapezoids) == 1:
             handle_case2(seg, affected_trapezoids[0])
+            visualizations.plot_dag(self, bbox)
             return
 
         left_point_region = self.find_point_region(self.head, seg.p1)
         right_point_region = self.find_point_region(self.head, seg.p2)
         affected_trapezoids.remove(left_point_region)
         affected_trapezoids.remove(right_point_region)
-        resultant_traps = handle_case1_left(seg.p1, seg, left_point_region)
+        degenerate_check_left = self.find_point(self.head, seg.p1)
+        resultant_traps = handle_case1_left(seg.p1, seg, left_point_region, degenerate_check_left)
+        visualizations.plot_dag(self, bbox)
 
         for trapezoid in affected_trapezoids:
             resultant_traps = handle_case3(seg, trapezoid, resultant_traps)
+            visualizations.plot_dag(self, bbox)
 
-        handle_case1_right(seg.p2, seg, right_point_region, resultant_traps)
+        degenerate_check_right = self.find_point(self.head, seg.p2)
+        handle_case1_right(seg.p2, seg, right_point_region, resultant_traps, degenerate_check_right)
+        visualizations.plot_dag(self, bbox)
 
+    def find_point(self, node, point):
+        curr_node = node.data
+
+        if isinstance(curr_node, Leaf):
+            return None
+
+        if isinstance(curr_node, PointNode) and curr_node.point.x == point.x and curr_node.point.y == point.y:
+            return node
+
+        left_point = self.find_point(curr_node.left, point)
+        if left_point is not None:
+            return left_point
+
+        return self.find_point(curr_node.right, point)
 
     def find_point_region(self, node, point):
         curr_node = node.data
