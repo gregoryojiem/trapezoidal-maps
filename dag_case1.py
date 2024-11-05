1ebfb3126c6bbb6169ebb4f07c8cd9e10c85c153
Gregory Ojiem <gro3228@rit.edu>
2024-11-05 14:36:24 -0500

    Case 1 refactored
    More robust now, in addition to some bug fixes

--- dag_case1.py
+++ dag_case1.py
@@ -5,72 +5,62 @@ from trapezoidal_map import Trapezoid
 def handle_case1_left(left_endpoint, seg, node_leaf, degenerate_point):
     """
     TODO
-    :param left_endpoint: Point in the trapezoid
-    :param seg: Segment that was added to the graph
-    :param node_leaf: Leaf containing the trapezoid
-    :returns: todo
     """
     trap = node_leaf.data.trap
-    if left_endpoint.is_above(trap.right_vert):
-        up_trap_r_bound = seg.p2 if not seg.p2.is_right_of(trap.top_seg.p2) else trap.top_seg.p2
+
+    up = Node(Leaf(Trapezoid(trap.top_seg, seg, left_endpoint, seg.p2)))
+    down = Node(Leaf(Trapezoid(seg, trap.bot_seg, left_endpoint, seg.p2)))
+
+    if not left_endpoint.is_above(trap.right_vert) or left_endpoint == trap.right_vert:
+        up.trap().right_vert = trap.right_vert
     else:
-        up_trap_r_bound = seg.p2 if not seg.p2.is_right_of(trap.right_vert) else trap.right_vert
-
-    up = Node(Leaf(Trapezoid(trap.top_seg, seg, left_endpoint, up_trap_r_bound)))
-
-    if left_endpoint.is_above(trap.right_vert):
-        down_trap_r_bound = seg.p2 if not seg.p2.is_right_of(trap.right_vert) else trap.right_vert
-    else:
-        down_trap_r_bound = seg.p2 if not seg.p2.is_right_of(trap.bot_seg.p2) else trap.bot_seg.p2
-
-    down = Node(Leaf(Trapezoid(seg, trap.bot_seg, left_endpoint, down_trap_r_bound)))
+        down.trap().right_vert = trap.right_vert
 
     s = Node(SegNode(up, down, seg))
-
-    lr_left = trap.top_seg.p1 if trap.top_seg.p1.is_right_of(trap.bot_seg.p1) else trap.bot_seg.p1
-    lr_right = left_endpoint
 
     if degenerate_point is not None:
         left_node = degenerate_point.data.left
     else:
-        left_node = Node(Leaf(Trapezoid(trap.top_seg, trap.bot_seg, lr_left, lr_right)))
+        left_node = Node(Leaf(Trapezoid(trap.top_seg, trap.bot_seg, trap.left_vert, left_endpoint)))
 
     p = PointNode(left_node, s, left_endpoint)
     node_leaf.data = p
     return [up, down]
 
 
-def handle_case1_right(point, seg, node_leaf, past_traps, degenerate_point):
+def handle_case1_right(right_endpoint, seg, node_leaf, past_traps, degenerate_point):
     """
     TODO
-    :param point: Point in the trapezoid
-    :param seg: Segment that was added to the graph
-    :param node_leaf: Leaf containing the trapezoid
-    :param past_traps: Trapezoids created that might be merged with this case
     """
-    trap = node_leaf.data.trap
+    if not node_leaf.top_seg().p1.is_above(seg.p1):
+        return
 
-    if past_traps[0].data.trap.top_seg == trap.top_seg:
+    trap = node_leaf.trap()
+
+    # merge this trapezoid with the past trapezoid if it's continuing along the same top segment
+    # then, extend right vertex of merged trapezoid
+    if past_traps[0].top_seg() == trap.top_seg:
         up = past_traps[0]
+        past_traps[0].data.trap.right_vert = right_endpoint
     else:
         up_left = seg.p1 if seg.p1.is_right_of(trap.left_vert) else trap.left_vert
-        up_right = point
-        up = Node(Leaf(Trapezoid(trap.top_seg, seg, up_left, up_right)))
+        up = Node(Leaf(Trapezoid(trap.top_seg, seg, up_left, right_endpoint)))
 
-    if past_traps[1].data.trap.bot_seg == trap.bot_seg:
+    # merge bottom trapezoid and extend right vertex
+    if past_traps[1].bot_seg() == trap.bot_seg:
         down = past_traps[1]
+        past_traps[1].data.trap.right_vert = right_endpoint
     else:
-        down_left = seg.p1 if seg.p1.is_right_of(trap.bot_seg.p1) else trap.bot_seg.p1
-        down_right = point
-        down = Node(Leaf(Trapezoid(seg, trap.bot_seg, down_left, down_right)))
+        down_left = seg.p1 if seg.p1.is_right_of(trap.left_vert) else trap.left_vert
+        down = Node(Leaf(Trapezoid(seg, trap.bot_seg, down_left, right_endpoint)))
 
+    # if there's a degenerate point, we don't need to trim the right trapezoid, we just assign it to this node
     if degenerate_point is not None:
         right_node = degenerate_point.data.right
     else:
-        right_right = trap.top_seg.p2 if not trap.top_seg.p2.x <= trap.bot_seg.p2.x else trap.bot_seg.p2
-        right_node = Node(Leaf(Trapezoid(trap.top_seg, trap.bot_seg, point, right_right)))
+        right_node = Node(Leaf(Trapezoid(trap.top_seg, trap.bot_seg, right_endpoint, trap.right_vert)))
 
-    s = Node(SegNode(up, down, seg))
-    p = PointNode(s, right_node, point)
-    node_leaf.data = p
+    segment = Node(SegNode(up, down, seg))
+    new_point = PointNode(segment, right_node, right_endpoint)
+    node_leaf.data = new_point
 
